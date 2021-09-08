1fe9fefb657033ad68bac76700462be0abf7d4ba
Nikola Pavicevic <nijepa@gmail.com>
2021-09-08 12:50:13 +0200

    take three

--- src/components/Music.vue
+++ src/components/Music.vue
@@ -1,15 +1,96 @@
 <template>
-  <div class="">
-    
-  </div>
+  <body>
+    <div class="layout">
+      <div class="title">
+        <h1>Title</h1>
+        <p>Description</p>
+      </div>
+
+      <nav>
+        <a href="index.php">Domů</a>
+        <a href="switcher.php">Menu 1</a>
+        <a href="#">Menu 2</a>
+        <a href="#">Menu 3</a>
+        <a href="#">Menu 4</a>
+        <a href="#">Menu 5</a>
+      </nav>
+
+      <main></main>
+
+      <div class="sidebar">
+        <div class="box_container">
+          <div class="box">
+            <div class="box_title">
+              <i class="material-icons">menu</i>Uživatelský panel
+            </div>
+            <div class="box_content">
+              <ul>
+                <li><a href="#">Můj profil</a></li>
+                <li><a href="#">Uživatelé</a></li>
+                <li><a href="#">Nastavení</a></li>
+                <li><a href="#">Odhlásit se</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="box">
+            <div class="box_title">
+              <i class="material-icons">menu</i>Admin panel
+            </div>
+            <div class="box_content">
+              <ul>
+                <li><a href="#">Správa uživatelů</a></li>
+                <li><a href="phpmyadmin">PhpMyAdmin</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="box">
+            <div class="box_title">
+              <i class="material-icons">people</i>Online uživatelé
+            </div>
+            <div class="box_content">
+              <ul>
+                <a href=""
+                  ><i class="material-icons box_online">circle</i>Admin</a
+                >
+                <a href=""
+                  ><i class="material-icons box_offline">circle</i>User_+</a
+                >
+                <a href=""
+                  ><i class="material-icons box_online">circle</i>sdfsdfdfds</a
+                >
+                <a href=""
+                  ><i class="material-icons box_online">circle</i>Admin</a
+                >
+                <a href=""
+                  ><i class="material-icons box_online">circle</i>Admin</a
+                >
+                <a href=""
+                  ><i class="material-icons box_online">circle</i>Admin</a
+                >
+                <a href=""
+                  ><i class="material-icons box_online">circle</i>Admin</a
+                >
+                <a href=""
+                  ><i class="material-icons box_online">circle</i>Admin</a
+                >
+                <a href=""
+                  ><i class="material-icons box_online">circle</i>Admin</a
+                >
+                <a href=""
+                  ><i class="material-icons box_online">circle</i>Admin</a
+                >
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer></footer>
+    </div>
+  </body>
 </template>
 
-<script>
-export default {
+<script></script>
 
-}
-</script>
-
-<style>
-
-</style>
+<style></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,37 +1,20 @@
 <template>
   <div id="mz-background">
-    <p>dsgsgdd</p>
+    <Music />
   </div>
 </template>
 <script>
+import Music from '../components/Music.vue'
 export default {
   name: "UnderworldPattern",
-  data() {
-    return {
-      currentTime: 0,
-    };
-  },
+  components: {Music}
+
   /* computed: {
   currentTime() {
     return Date.now();
   },
 }, */
-  mounted: function() {
-    window.setInterval(() => {
-      this.currentTime = new Date();
-    }, 1000);
-  },
-  unmounted() {
-    window.clearInterval();
-  },
 
-  watch: {
-    async currentTime(newValue, oldValue) {
-      // Log user out when current time exceeds the token expiry time
-      // Getting token ExpiryTime
-      console.log(newValue + " " + oldValue);
-    },
-  },
 };
 </script>
 <style lang="scss" scoped></style>
